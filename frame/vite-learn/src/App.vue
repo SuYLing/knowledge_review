6dee0cf06660ea50d627d4a3a961250671b46313
yuling <sakuriget@gmail.com>
2025-12-24 20:40:08 +0800

    vite learn

---
+++ frame/vite-learn/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<h1 class="title">{{ count }}</h1>
+	<button @click="count++">+</button>
+	<button @click="count--">-</button>
+</template>
+
+<script lang="ts" setup>
+	import { ref } from 'vue'
+
+	const count = ref(0)
+</script>
+
+<style>
+	.title {
+		color: red;
+	}
+	button {
+		margin: 0 5px;
+		outline: none;
+		border: none;
+		padding: 5px 10px;
+		background-color: #409eff;
+		color: #fff;
+	}
+</style>
+
