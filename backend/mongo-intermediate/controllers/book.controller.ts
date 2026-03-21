591da9d3976a9338d04679295e2bc72240d4c32d
yuling <sakuriget@gmail.com>
2026-03-21 15:38:25 +0800

    backend:mongo:feat: complete mongo basic

--- backend/mongo-intermediate/controllers/book.controller.ts
+++ backend/mongo-intermediate/controllers/book.controller.ts
@@ -1,8 +1,84 @@
 import type { Handler } from "express";
+import { AuthorModel } from "../models/author.js";
+import { BookModel } from "../models/book.js";
 
-export const createAuthor:Handler = async (req,res)=>{
+export const createBook: Handler = async (req, res) => {
   try {
-    
+    const { title, author } = req.body
+    const newBook = await BookModel.create({
+      author,
+      title
+    })
+    if (newBook) {
+      return res.status(201).json({
+        success: true,
+        message: 'book created successfully',
+        data: newBook
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      success: false,
+      message: 'something went wrong! Please try again',
+    })
+  }
+}
+export const createAuthor: Handler = async (req, res) => {
+  try {
+    const { name, bio } = req.body
+    const newAuthor = await AuthorModel.create({
+      name, bio
+    })
+    if (newAuthor) {
+      return res.status(201).json({
+        success: true,
+        message: 'author created successfully',
+        data: newAuthor
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      success: false,
+      message: 'something went wrong! Please try again',
+    })
+  }
+}
+
+export const getBookWithAuthor: Handler = async (req, res) => {
+  try {
+    const id: string = req.params.id as string
+    const book = await BookModel.findById(id).populate('author')
+    if (book) {
+      return res.status(200).json({
+        success: true,
+        message: 'books fetched successfully',
+        data: book
+      })
+    } else {
+      return res.status(404).json({
+        success: false,
+        message: 'book not found',
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      success: false,
+      message: 'something went wrong! Please try again',
+    })
+  }
+}
+
+export const getAllBooks: Handler = async (req, res) => {
+  try {
+    const books = await BookModel.find({})
+    res.status(200).json({
+      success: true,
+      message: 'books fetched successfully',
+      data: books
+    })
   } catch (error) {
     console.log(error)
     res.status(500).json({
--- backend/mongo-intermediate/routes/book.route.ts
+++ backend/mongo-intermediate/routes/book.route.ts
@@ -1,5 +1,8 @@
 import express, { type Router } from "express";
-import { createAuthor } from "../controllers/book.controller.js";
+import { createAuthor, createBook, getAllBooks, getBookWithAuthor } from "../controllers/book.controller.js";
 
 export const bookRouter: Router = express.Router()
-bookRouter.post("/add", createAuthor)
+bookRouter.post("/add-author", createAuthor)
+bookRouter.post("/add-book", createBook)
+bookRouter.get("/get/book/:id", getBookWithAuthor)
+bookRouter.get("/get/books", getAllBooks)
