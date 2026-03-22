da831021efa5b41a24a0cbc4e13a96d58bff3296
yuling <sakuriget@gmail.com>
2026-03-22 10:30:52 +0800

    backend:socket: socket complete

--- backend/socket/index.ts
+++ backend/socket/index.ts
@@ -10,16 +10,26 @@ app.use(express.static("./public"))
 const server = http.createServer(app)
 
 const io = new Server(server)
-server.listen(3000, () => console.log('server on 3000'))
+server.listen(3000, () => console.log('server on http://localhost:3000'))
 const users = new Set()
 io.on('connection', (socket) => {
   console.log('a user connected');
   // 加入用户
   socket.on("join", (username) => {
-    console.log(username)
     users.add(username)
-    socket.emit("joined", Array.from(users).filter(user => user !== username))
-
+    socket.username = username
+    io.emit("joined", username)
+    io.emit("user-list", Array.from(users))
   })
-
+  // 用户退出
+  socket.on("leave", () => {
+    console.log("socket.id", socket.username)
+    users.delete(socket.username)
+    io.emit("left", socket.username)
+    io.emit("user-list", Array.from(users))
+  })
+  socket.on("chat message", (info) => {
+    console.log(info)
+    io.emit("chat message", info)
+  })
 });
