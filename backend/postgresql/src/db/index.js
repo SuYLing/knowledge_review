92b616be0f57352a3f3e456a24e2abba8780e474
yuling <sakuriget@gmail.com>
2026-03-31 10:34:40 +0800

    backend:postgres: init postgres env

---
+++ backend/postgresql/src/db/index.js
@@ -0,0 +1,22 @@
+import { Pool } from "pg"
+
+const pool = new Pool({
+  connectionString: "postgre://postgres:123yuling@localhost:5432",
+  password: "123yuling"
+})
+pool.connect()
+pool.on("error", (err) => console.log("something error: ", err))
+pool.on("connect", (client) => {
+  console.log("db connected successful")
+})
+export async function query(text, params) {
+  const start = Date.now()
+  try {
+    const result = await pool.query(text, params)
+    const duration = Date.now() - start
+    console.log(`Executed query: ${{ text, duration, rows: result.rowCount }}`)
+  }
+  catch (err) {
+    console.error("something error: ", err)
+  }
+}
