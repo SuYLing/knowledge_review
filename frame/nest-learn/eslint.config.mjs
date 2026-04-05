e54a69a8d09d9b9002372dbd4e4dc1ac21b27e96
yuling <sakuriget@gmail.com>
2026-04-05 15:55:43 +0800

    frame:nest-js: complete prisma's config

--- frame/nest-learn/eslint.config.mjs
+++ frame/nest-learn/eslint.config.mjs
@@ -1,35 +1,36 @@
 // @ts-check
 import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import { defineConfig } from "eslint/config";
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-
-export default tseslint.config(
+export default defineConfig(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ["./eslint.config.mjs", "./prisma.config.ts"]
   },
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  eslintConfigPrettier,
   {
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.jest,
       },
-      ecmaVersion: 5,
-      sourceType: 'module',
+    }
+  },
+  {
+    languageOptions: {
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: import.meta.dirname,
       },
     },
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    },
-  },
-);
+      "@typescript-eslint/no-floating-promises": "off",
+      "@typescript-eslint/no-extraneous-class": "off",
+    }
+  }
+)
--- frame/nest-learn/src/app.service.ts
+++ frame/nest-learn/src/app.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from './modules/prisma/prisma.service'
 
 @Injectable()
 export class AppService {
+  constructor(private readonly prismaService: PrismaService) {}
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!'
   }
 }
