b002547097a0fb029efe1d19f34f8cf94764267c
yuling <sakuriget@gmail.com>
2026-04-06 15:18:15 +0800

    frame:nest-learn: replace process to configModule

--- frame/nest-learn/src/shared/shared.module.ts
+++ frame/nest-learn/src/shared/shared.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { JwtModule } from '@nestjs/jwt'
 import joi from 'joi'
 import PrismaModule from './prisma/prisma.module'
@@ -11,12 +11,21 @@ import PrismaModule from './prisma/prisma.module'
         NODE_ENV: joi.string().valid('development', 'test', 'production').default('test'),
         DATABASE_URL: joi.string(),
         PORT: joi.number().default(3000),
+        JWT_SECRET: joi.string().default('123yuling'),
       }),
     }),
-    JwtModule.register({
-      global: true,
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '12h' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory(configService: ConfigService) {
+        return {
+          global: true,
+          signOptions: {
+            expiresIn: '12h',
+          },
+          secret: configService.get<string>('JWT_SECRET', { infer: true }),
+        }
+      },
     }),
     PrismaModule,
   ],
