c345a43eafe6c97b621f6512b1a0dcf43915b5e9
yuling <sakuriget@gmail.com>
2026-05-01 10:55:53 +0800

    frame:next-learn: add cache

--- frame/next-learn/lib/get-session.ts
+++ frame/next-learn/lib/get-session.ts
@@ -1,9 +1,10 @@
 import { headers } from 'next/headers'
+import { cache } from 'react'
 import { auth } from './auth'
 /**
  * 获取 服务端获取 session
- * */ 
-export const getSession = async () => {
+ * */
+export const getSession = cache(async () => {
 	return await auth.api.getSession({ headers: await headers() })
-}
+})
 
